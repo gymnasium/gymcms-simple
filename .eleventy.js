9dacc825c7aa09143a03d800aebce54352b9d577
jgagne <justin.gagne@montserrat.edu>
2023-11-02 21:39:17 +0100

    Update includes
    - Add layouts folder
    - Add layout alias

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./css/');
   eleventyConfig.addWatchTarget('./css/');
+  eleventyConfig.addLayoutAlias('courses.html', 'layouts/courses.html');
   return {
   }
 };
